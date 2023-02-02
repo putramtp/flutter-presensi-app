3fdd68425542d6e10178dd824908336dfba08ac9
Muhammad Saiful Anwar <msaiflanwr@gmail.com>
2023-02-02 12:47:13 +0700

    Update 43 - Start Open Release (Error Handling network-request-failed)

--- lib/app/modules/all_presensi/views/all_presensi_view.dart
+++ lib/app/modules/all_presensi/views/all_presensi_view.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:get/get.dart';
 import 'package:presensi/app/routes/app_pages.dart';
@@ -11,119 +12,131 @@ class AllPresensiView extends GetView<AllPresensiController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Semua Presensi'),
+        leading: IconButton(
+          onPressed: () => Routes.HOME,
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            size: 14,
+          ),
+          color: Color(0xff333333),
+        ),
+        title: Text(
+          'Semua Presensi',
+          style: GoogleFonts.poppins(
+              color: Color(0xff333333),
+              fontSize: 16,
+              fontWeight: FontWeight.w600),
+        ),
         centerTitle: true,
+        backgroundColor: Color(0xffFFC107),
       ),
       body: GetBuilder<AllPresensiController>(
         builder: (c) => FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        future: controller.getPresence(),
-        builder: (context, snap) {
-          if (snap.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+            future: controller.getPresence(),
+            builder: (context, snap) {
+              if (snap.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-          if (snap.data?.docs.length == 0 || snap.data?.docs.length == null){
-              return SizedBox(
-                height: 60,
-                child: Center(
-                  child: Text("Belum ada data"),
-                ),
-              );
-            }
+              if (snap.data?.docs.length == 0 ||
+                  snap.data?.docs.length == null) {
+                return SizedBox(
+                  height: 60,
+                  child: Center(
+                    child: Text("Belum ada data"),
+                  ),
+                );
+              }
 
-          return ListView.builder(
-                    padding: EdgeInsets.all(20),
-                    itemCount: snap.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      Map<String, dynamic> data = snap.data!.docs[index].data();
+              return ListView.builder(
+                  padding: EdgeInsets.all(20),
+                  itemCount: snap.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> data = snap.data!.docs[index].data();
                     return Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Material(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(15),
-                    child: InkWell(
-                      onTap: ()=> Get.toNamed(
-                        Routes.DETAIL_PRESENSI, 
-                        arguments: data
-                        ),
-                      borderRadius: BorderRadius.circular(15),
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Material(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15),
+                        child: InkWell(
+                          onTap: () => Get.toNamed(Routes.DETAIL_PRESENSI,
+                              arguments: data),
+                          borderRadius: BorderRadius.circular(15),
+                          child: Container(
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  "Datang",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Datang",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "${DateFormat.yMMMEd().format(DateTime.parse(data['date']))}",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                Text(data['datang']?['date'] == null
+                                    ? "-"
+                                    : "${DateFormat.jms().format(DateTime.parse(data['datang']!['date']))}"),
+                                SizedBox(
+                                  height: 10,
                                 ),
                                 Text(
-                                  "${DateFormat.yMMMEd().format(DateTime.parse(data['date']))}",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  ),
+                                  "Pulang",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
                                 ),
+                                Text(data['pulang']?['date'] == null
+                                    ? "-"
+                                    : "${DateFormat.jms().format(DateTime.parse(data['pulang']!['date']))}"),
                               ],
                             ),
-                            Text(data['datang']?['date'] == null ? "-" : "${DateFormat.jms().format(DateTime.parse(data['datang']!['date']))}" ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              "Pulang",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            Text(data['pulang']?['date'] == null ? "-" : "${DateFormat.jms().format(DateTime.parse(data['pulang']!['date']))}"),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                );
-                    }
-                  );
-        }
+                    );
+                  });
+            }),
       ),
-      ),
-
-
       floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              Get.dialog(
-                Dialog(
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    height: 400,
-                    child: SfDateRangePicker(
-                      monthViewSettings: DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
-                      selectionMode: DateRangePickerSelectionMode.range,
-                      showActionButtons: true,
-                      onCancel: ()=> Get.back(),
-                      onSubmit: (obj) {
-                        if (obj != null){
-                          if ((obj as PickerDateRange).endDate != null) {
-                            controller.pickDate(obj.startDate!, obj.endDate!);
-                          }
+        onPressed: () {
+          Get.dialog(
+            Dialog(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                height: 400,
+                child: SfDateRangePicker(
+                    monthViewSettings:
+                        DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
+                    selectionMode: DateRangePickerSelectionMode.range,
+                    showActionButtons: true,
+                    onCancel: () => Get.back(),
+                    onSubmit: (obj) {
+                      if (obj != null) {
+                        if ((obj as PickerDateRange).endDate != null) {
+                          controller.pickDate(obj.startDate!, obj.endDate!);
                         }
                       }
-                    ),
-                  ),
-                ),
-                );
-            },
-            child: Icon(Icons.format_list_bulleted_rounded),
-        ),
+                    }),
+              ),
+            ),
+          );
+        },
+        child: Icon(Icons.search),
+        backgroundColor: Color(0xffFFC107),
+      ),
     );
   }
 }
